87e6f4542e12edd1ebbbae4e4e5f902a9892d2fa
Jake Trimble <zjatreturns@gmail.cm>
2016-03-12 10:40:20 -0700

    profile controller built out

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,13 +1,28 @@
 class ProfilesController < ApplicationController
-  def index
-  end
+  before_action :profile
 
   def show
   end
 
-  def new
-  end
-
   def edit
   end
+
+  def update
+    if @profile.update(profile_params)
+      flash[:notice] = 'Profile updated successfully!'
+      redirect_to user_profile_path(current_user, @profile)
+    else
+      flash[:alert] = 'Profile not updated!'
+      render :edit
+    end
+  end
+
+  private
+    def profile_params
+      params.require(:profile).permit(:bio, :about_me, :bg_pic, :gender, :age)
+    end
+
+    def profile
+      @profile = current_user.profile
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,4 @@
 Rails.application.routes.draw do
-  get 'profiles/index'
-
-  get 'profiles/show'
-
-  get 'profiles/new'
-
-  get 'profiles/edit'
-
-  get 'languages/index'
-
-  get 'languages/show'
-
-  get 'languages/new'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/edit'
-
-  get 'homes/index'
-
   devise_for :users
   root "home#index"
 
