d7f1aa3f49a710a9edb1c514243f72d2c7d31612
Jake Trimble <zjatreturns@gmail.cm>
2016-03-12 10:10:36 -0700

    added 4 controllers

---
+++ app/controllers/languages_controller.rb
@@ -0,0 +1,10 @@
+class LanguagesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,33 @@
 Rails.application.routes.draw do
+  get 'profiles/index'
+
+  get 'profiles/show'
+
+  get 'profiles/new'
+
+  get 'profiles/edit'
+
+  get 'languages/index'
+
+  get 'languages/show'
+
+  get 'languages/new'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'homes/index'
+
   devise_for :users
   root "home#index"
 
   resources :user do
-    resoureces :language
-    resoureces :profile
+    resources :language
+    resources :profile
   end
 end
